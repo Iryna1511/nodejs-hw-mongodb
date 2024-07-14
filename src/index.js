75173522d722dc8a5477b3370428482c82dc2269
Iryna1511 <148438865+Iryna1511@users.noreply.github.com>
2024-07-15 00:23:55 +0300

    up

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { stsrtServer } from './server';
+
+stsrtServer();
---
+++ src/server.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+
+const PORT = 3000;
+
+export const stsrtServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  //   app.use((req, res, next) => {
+  //     console.log(`Time: ${new Date().toLocaleString()}`);
+  //     next();
+  //   });
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello world!',
+    });
+  });
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
