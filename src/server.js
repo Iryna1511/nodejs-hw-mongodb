cb7bded5e7d039326f0a688534b18264645101ec
Iryna1511 <148438865+Iryna1511@users.noreply.github.com>
2024-07-16 12:06:27 +0300

    fix status

--- src/server.js
+++ src/server.js
@@ -34,6 +34,7 @@ export const startServer = () => {
   app.get("/contacts", async (req, res) => {
     const contacts = await getAllContacts();
     res.status(200).json({
+      status: 200,
       message: "Successfully found contacts!",
       data: contacts,
     });
